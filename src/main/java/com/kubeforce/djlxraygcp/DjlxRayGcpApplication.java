b8f9267d0385c57ab1cf8282aa19415dc653b46d
banup-kubeforce <102495939+banup-kubeforce@users.noreply.github.com>
2022-09-22 06:01:31 -0500

    Initial commit

---
+++ src/main/java/com/kubeforce/djlxraygcp/DjlxRayGcpApplication.java
@@ -0,0 +1,13 @@
+package com.kubeforce.djlxraygcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjlxRayGcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjlxRayGcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubeforce/djlxraygcp/DjlxRayGcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubeforce.djlxraygcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjlxRayGcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
